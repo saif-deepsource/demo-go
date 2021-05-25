763bcf3787f6a07e74cd37de97d436e5fb6a03dc
Saif Sadiq <72203506+saif-deepsource@users.noreply.github.com>
2021-05-25 21:44:13 +0530

    Update unsafe_example.go

--- unsafe_example.go
+++ unsafe_example.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"fmt"
+	
+	
+	
+	
 	"unsafe"
 )
 
 type Fake struct{}
 
-func (Fake) Good() {}
+func 
+
+(Fake) Good() {}
 
 func unsafeCode() {
 	unsafeM := Fake{}
-	unsafeM.Good()
+	unsafeM.Good(
+	
+	
+	)
 	intArray := [...]int{1, 2}
 	fmt.Printf("\nintArray: %v\n", intArray)
 	intPtr := &intArray[0]
 	fmt.Printf("\nintPtr=%p, *intPtr=%d.\n", intPtr, *intPtr)
-	addressHolder := uintptr(unsafe.Pointer(intPtr)) + unsafe.Sizeof(intArray[0])
+	addressHolder := uintptr(unsafe.Pointer
+				 
+				 
+				 (intPtr)) + unsafe.Sizeof(intArray[0])
 	intPtr = (*int)(unsafe.Pointer(addressHolder))
 	fmt.Printf("\nintPtr=%p, *intPtr=%d.\n\n", intPtr, *intPtr)
 }
